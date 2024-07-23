5e45a4223d8670b2a9c513759ac9f5395db58732
PAVANDEVMURARI <23dcs022@charusat.edu.in>
2024-07-23 10:21:11 +0530

    Add files via upload

---
+++ Practical11.java
@@ -0,0 +1,8 @@
+public class prac_11 {
+    public static void main(String[] args) {
+        String st=new String("CHARUSAT UNIVESITY");
+        System.out.println(st.length());
+        System.out.println(st.replace('H','P'));
+        System.out.println(st.toLowerCase());
+    }
+}
